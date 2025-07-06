f355f29d644334600264939c1da41dd92330bd7d
ees <21estankevich@gmail.com>
2025-07-06 19:58:56 +0200

    Change scheduled Task name and time

--- createScheduledTask.ps1
+++ createScheduledTask.ps1
@@ -1,4 +1,4 @@
-$trigger = New-ScheduledTaskTrigger -At 8pm -Daily
+$trigger = New-ScheduledTaskTrigger -At 8am -Daily
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"C:\repos\time.ps1`""
 $settings = New-ScheduledTaskSettingsSet
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Write-Host Time" -Description "Prints time in console" -Settings $settings
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "printTime" -Description "Prints time in console" -Settings $settings
