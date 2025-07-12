79aea190e3a74bbd3e2dedc9ed064d882957bfd8
ees <21estankevich@gmail.com>
2025-07-12 13:15:55 +0200

    Add newlines and date information to gui

--- gui.ps1
+++ gui.ps1
@@ -4,9 +4,11 @@ Add-Type -AssemblyName System.Drawing
 
 $weather = Invoke-RestMethod "wttr.in/?format=3"
 $form = New-object System.Windows.Forms.Form
+$Date = Get-Date
+$Time = "$Date `n `n"
 
 $button = New-Object System.Windows.Forms.Button
-$button.Text = $weather 
+$button.Text = $Time, $weather
 $button.Size = New-Object System.Drawing.Size(222,222)
 
 
