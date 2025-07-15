b6dddfb025a9551a5471e8496ff648c078f2b57e
21estankevich@gmail.com <21estankevich@gmail.com>
2025-07-15 15:52:17 +0200

    Add 9 buttons and grid them

---
+++ material/eachButton.ps1
@@ -0,0 +1,30 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -Assemblyname System.Drawing
+
+
+$rRay = @(1,2,3,4,5,6,7,8,9)
+$gridx = @(0, 50, 100, 0, 50, 100, 0, 50, 100)
+$gridy = @(0, 0, 0, 50, 50, 50, 100, 100, 100)
+$y = 0
+
+$form = New-Object System.Windows.Forms.Form
+
+foreach ($item in $rRay) {
+    $button = New-Object System.Windows.Forms.Button
+    $button.Size = New-Object System.Drawing.Size(50,50)
+    $button.Text = $item
+    $button.Location = New-Object System.Drawing.Point(($gridx[$i] + $x), ($gridy[$i]+$y))
+
+$form.Controls.Add($button)
+
+$y = $y + 0
+$x = $x + 0
+$i = $i + 1
+
+#foreach = $form.Controls.add(#)
+}
+
+[void]$form.ShowDialog()
+
+
+
