67407d46f61f159038bd1f071ecaf3bafe51a5c0
Erik Stankevich | MikroPlan GmbH <ees@mikroplan.com>
2025-07-16 18:56:30 +0200

    button finally moves... ... ...

--- material/movingButton.ps1
+++ material/movingButton.ps1
@@ -6,20 +6,25 @@ $canvas = New-Object System.Windows.Controls.Canvas
     $button.Width = 100
     $button.Height = 50 
     
-    $x = 0
+    $script:x = 0
     [System.Windows.Controls.Canvas]::SetLeft($button, $x)
     [System.Windows.Controls.Canvas]::SetTop($button, 0)
 
 
     $canvas.Children.Add($button)
 
+
+    $timer = New-Object System.Windows.Threading.DispatcherTimer
+    $timer.Interval = [TimeSpan]::FromSeconds(1)
+    $timer.Add_Tick({
+        $script:x = $script:x + 10
+        [System.Windows.Controls.Canvas]::SetLeft($button, $script:x)
+    })
+
+    $timer.Start()
+
     $form.Content = $canvas
 $form.ShowDialog()
 
-    while ($true) {
-        $x = $x + 10
-        [System.Windows.Controls.Canvas]::SetLeft($button, $x)
-        Start-Wait -Seconds 1
-    }
 
     
