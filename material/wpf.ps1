e3e8b4facde9bee6fd9f4bb10b5d6e9f199a66cf
21estankevich@gmail.com <21estankevich@gmail.com>
2025-07-16 12:57:18 +0200

    Create two buttons in wpf

---
+++ material/wpf.ps1
@@ -0,0 +1,22 @@
+Add-Type -AssemblyName PresentationFramework
+
+$window = New-Object System.Windows.Window
+
+$stack = New-Object System.Windows.Controls.StackPanel
+$stack.Orientation = "Horizontal"
+
+$button = New-Object System.Windows.Controls.Button
+$button.Content = "foo"
+$button.Width = 50
+$button.Height = 30
+
+$btn2 = New-Object System.Windows.Controls.Button
+$btn2.Content = "bar"
+$btn2.Width = 50
+$btn2.Height = 30
+
+$stack.Children.Add($button)
+$stack.Children.Add($btn2)
+
+$window.Content = $stack
+$window.ShowDialog()
