dbe4d91a066e99cadbe03620b2e9882472f6ac23
21estankevich@gmail.com <21estankevich@gmail.com>
2025-07-16 15:35:28 +0200

    Make use of script: which saves me

--- material/wpf.ps1
+++ material/wpf.ps1
@@ -9,11 +9,17 @@ $button = New-Object System.Windows.Controls.Button
 $button.Content = "foo"
 $button.Width = 50
 $button.Height = 30
+$button.add_Click({
+    $button.Content = "lorem"
+})
 
 $btn2 = New-Object System.Windows.Controls.Button
 $btn2.Content = "bar"
 $btn2.Width = 50
 $btn2.Height = 30
+$btn2.add_Click({
+    $btn2.Content = "ipsum"
+})
 
 $stack.Children.Add($button)
 $stack.Children.Add($btn2)
