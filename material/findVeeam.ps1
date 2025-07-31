7b604d87b3f5c05d9794f3ea07659b774df5de6a
Erik Stankevich | MikroPlan GmbH <ees@mikroplan.com>
2025-07-31 09:04:17 +0200

    Filter for Failed in Subject, filter for NOT success in subject and filter for veeam in body, if all true, write host.

--- material/findVeeam.ps1
+++ material/findVeeam.ps1
@@ -1,8 +1,14 @@
 $outlook = New-Object -ComObject Outlook.Application
 $namespace = $outlook.GetNamespace("MAPI")
 $OO = $namespace.folders | Where-Object {$_.Name -like "*ffentliche Ordner*"}
-$Alle = $OO.folder | Where-Object {$_.Name -like "*ffentliche Ordner*"}
+$Fav = $OO.folders | Where-Object {$_.Name -like "*Favoriten*"}
+$Monitoring = $Fav.folders | Where-Object {$_.Name -like "*Monitoring*"}
+$Items = $Monitoring.Items
+$Veeam = $Items | Where-Object {$_.Subject -like "*[Failed]*"} 
+$Body = $Veeam | Where-Object {$_.Body -like "*veeam*"}
+$Unlike = $Body | Where-Object {$_.Subject -notlike "*success*"}
 
-foreach ($item in $OO.folders){
-    Write-Host $item.name
+
+foreach ($item in $Unlike){
+    Write-Host $item.Subject
 }
